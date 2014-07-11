b2621e205da6b13a72b037ab9e48c438b205b563
Michael Huynh <mike.huynh@gmail.com>
2014-07-11 11:34:54 -0400

    Fix: bind only port, not IP address.

--- ircd.js
+++ ircd.js
@@ -7,7 +7,7 @@ var http_port = Number(process.env.PORT || 5000);
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!');
-}).listen(http_port, '127.0.0.1');
+}).listen(http_port);
 console.log('http server running');
 
 
